1cda6030ea8e6300abab61b3335f81882d80bd29
Babyhamsta <22938086+Babyhamsta@users.noreply.github.com>
2025-07-04 23:49:16 -0500

    Update carstate.py
    Added FaultedNonCritical for BRAKE_MODULE

--- opendbc_repo/opendbc/car/honda/carstate.py
+++ opendbc_repo/opendbc/car/honda/carstate.py
@@ -100,7 +100,13 @@ class CarState(CarStateBase, CarStateExt):
     else:
       # On some cars, these two signals are always 1, this flag is masking a bug in release
       # FIXME: find and set the ACC faulted signals on more platforms
-      if self.CP.openpilotLongitudinalControl:
+<<<<<<< HEAD
+      if self.CP.openpilotLongitudinalControl and self.CP.carFingerprint in HONDA_BOSCH_CANFD:
+        if self.CP.flags & HondaFlags.BOSCH_ALT_BRAKE:
+          ret.carFaultedNonCritical = bool(cp.vl["BRAKE_MODULE"]["BRAKE_ERROR"])
+        else:
+          ret.carFaultedNonCritical = bool(cp.vl["BRAKE_ERROR"]["BRAKE_ERROR_1"] or cp.vl["BRAKE_ERROR"]["BRAKE_ERROR_2"])
+      elif self.CP.openpilotLongitudinalControl:
         ret.accFaulted = bool(cp.vl["STANDSTILL"]["BRAKE_ERROR_1"] or cp.vl["STANDSTILL"]["BRAKE_ERROR_2"])
 
       # Log non-critical stock ACC/LKAS faults if Nidec (camera)
